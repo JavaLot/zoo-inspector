5d23fc503c9c58708d1895aba50b91da9a8e2a85
Michael Rodionov <54245754+JavaLot@users.noreply.github.com>
2023-03-05 13:01:27 +0300

    Logging corrections

--- src/main/java/org/apache/zookeeper/inspector/logger/LoggerFactory.java
+++ src/main/java/org/apache/zookeeper/inspector/logger/LoggerFactory.java
@@ -21,18 +21,14 @@ import org.slf4j.Logger;
 
 /**
  * Provides a {@link Logger} for use across the entire application
- * 
  */
-public class LoggerFactory
-{
-	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger("org.apache.zookeeper.inspector"); //$NON-NLS-1$
+public class LoggerFactory {
+	private static final Logger logger = org.slf4j.LoggerFactory.getLogger("org.apache.zookeeper.inspector");
 
 	/**
 	 * @return {@link Logger} for ZooInspector
 	 */
-	public static org.slf4j.Logger getLogger()
-	{
+	public static Logger getLogger() {
 		return logger;
 	}
-
 }
